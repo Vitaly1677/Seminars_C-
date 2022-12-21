b351401ac0c58ed483f29eb9579fe8ca02f0b88a
Git GeekPythonUser <geekpython@yandex.ru>
2022-12-21 17:32:06 +0300

    Завершил Task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Текст задания
+// для исполнения
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
