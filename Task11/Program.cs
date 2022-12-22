df69d318142ae8b5c5f0673f0c17a42fafb280f1
Git GeekPythonUser <geekpython@yandex.ru>
2022-12-22 14:37:29 +0300

    Добавил решени Task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 11. Напишите программу, которая 
+// выводит случайное трёхзначное число и 
+// удаляет вторую цифру этого числа. 
+// 456 -> 46 
+// 782 -> 72 
+// 918 -> 98
+
+int randomNumber = new Random().Next(100, 1000);
+int firstNumber = (randomNumber - randomNumber % 100) / 100;
+int thirdNumber = randomNumber % 100 % 10;
+Console.WriteLine($"{randomNumber} -> {firstNumber}{thirdNumber}");
