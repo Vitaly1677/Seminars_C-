e5f902bfe68172d84b5c2d3b9b16f71063630163
Git GeekPythonUser <geekpython@yandex.ru>
2022-12-22 15:33:01 +0300

    Добавил решение Task12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 12. Напишите программу, которая будет 
+// принимать на вход два числа и 
+// выводить, является ли первое число кратным второму. 
+// Если число 1 не кратно числу 2, то программа 
+// выводит остаток от деления. 
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+Console.Write("Введите 1-e число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-e число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int remainder = firstNumber % secondNumber;
+if (remainder == 0) Console.WriteLine($"{firstNumber}, {secondNumber} -> кратно");
+else Console.WriteLine($"{firstNumber}, {secondNumber} -> {remainder}");
