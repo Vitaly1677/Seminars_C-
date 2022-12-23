6229e915c931a1220339888fb3a85406660e719b
Git GeekPythonUser <geekpython@yandex.ru>
2022-12-23 10:56:39 +0300

    Добавил решение Task14 посредствам метода

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -5,3 +5,10 @@
 // 46 -> нет 
 // 161 -> да
 
+Console.Write("Введите число, для проверки на кратность 7 и 23: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(BothMultiple(number) ? "Да" : "Нет");
+bool BothMultiple(int num, int div1 = 7, int div2 = 23)
+{
+ return num % div1 ==0 && num % div2 == 0;
+}
