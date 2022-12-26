8f0d450cc9dca38c8e7da1def431dc6ec96a80bd
Git GeekPythonUser <geekpython@yandex.ru>
2022-12-26 19:42:32 +0300

    Добавленно решение Task17

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -3,3 +3,23 @@
 // причем X != 0, Y != 0
 //выдает номер четверти плоскости, в которой находится эта точка.
 
+Console.WriteLine("Введите координаты точки");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter (x, y);
+string result = quarter > 0 
+                ? $"Указанные координаты соотв. четверти -> {quarter}"
+                : "Введены некорректные координаты";
+Console.WriteLine(result);
+
+int Quarter(int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return 1;
+    if (xc < 0 && yc > 0) return 1;
+    if (xc < 0 && yc < 0) return 1;
+    if (xc > 0 && yc < 0) return 1;
+    return 0;
+}
