bd72aa021fe3b363a970f8a4537d5bc1b986a22b
Git GeekPythonUser <geekpython@yandex.ru>
2022-12-27 20:18:25 +0300

    создал Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+Console.Write("Введите число (N) для выдачи таблицы квадратов чисел от 1 до N");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0; 
+
+while (i > number)
+{
+    Console.WriteLine(Square(number));
+    i++;
+}
+
+int Square(int num)
+{
+    return  num * num;
+}
