9a3b192f9e5a25560bbb9950f7a2dbaabaf20e32
Git GeekPythonUser <geekpython@yandex.ru>
2022-12-27 18:21:19 +0300

    Поправил Task18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -2,19 +2,17 @@
 // заданному номеру четверти, показывает диапазон
 // возможных координат точек в этой четверти (x и y).
 
-Console.Write("Введите четверть: ");
-int quarter = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер четверти: ");
+string quarter = Console.ReadLine();
 
-string result = Coordinates(quarter) != "Err"
-                ? $"Диапазон возможных координат точек в четверти {quarter} -> {Coordinates(quarter)}"
-                : "Введены некорректные данные.";
+string result = Coordinates(quarter);
 Console.WriteLine(result);
 
-string Coordinates(int qurt)
+string Coordinates(string qurt)
 {
-    if (quarter == 1) return "x > 0, y > 0";
-    if (quarter == 2) return "x < 0, y > 0";
-    if (quarter == 3) return "x < 0, y < 0";
-    if (quarter == 4) return "x > 0, y < 0";
-    return "Err";
+    if (quarter == "1") return "x > 0, y > 0";
+    if (quarter == "2") return "x < 0, y > 0";
+    if (quarter == "3") return "x < 0, y < 0";
+    if (quarter == "4") return "x > 0, y < 0";
+    return "Некорректные данные";
 }
