07ea809ce8f196b9478cde51e1237edf42cf5e58
Git GeekPythonUser <geekpython@yandex.ru>
2023-01-18 19:17:51 +0300

    Добавил решение задачи Task30

--- Task30_Массив_Ввод_Вывод/Program.cs
+++ Task30_Массив_Ввод_Вывод/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+int[] array = new int[8];
+ArrayAddRandomNum();
+for (int i = 0; i < array.Length; i++)
+{
+   Console.WriteLine(array[i]); 
+}
+
+
+void ArrayAddRandomNum()
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 2);
+//        Console.WriteLine(array[i]); 
+    }
+}
+
