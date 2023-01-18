2bfe8e8e0360e8b6f725c946cf868c4865aab9fc
Git GeekPythonUser <geekpython@yandex.ru>
2023-01-18 17:42:38 +0300

    Добавил решение задачи Task28

---
+++ Task28_Произведение_чисел/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int multiplyNumber = MultiplyNumber(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} -> {multiplyNumber}");
+
+int MultiplyNumber(int count)
+{
+    int num = 1;
+    for (int i = 1; i < count; i++)
+    {
+        num = num * (i+1);
+    }
+    return num;
+}
