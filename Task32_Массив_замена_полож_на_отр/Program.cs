c8833284512dc4c93863b0a74cdcd0c6330b3112
Vitaly Adass <geekpython16@yandex.ru>
2023-01-23 15:39:30 +0300

    Добавил решение Task32

---
+++ Task32_Массив_замена_полож_на_отр/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 32: Напишите программу замены элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] array = new int[]{-4, -8, 8, 2};
+PrintArray(array);
+Console.Write(" -> ");
+array =  InvertArray(array);
+PrintArray(array);
+Console.WriteLine("");
+
+int[] InvertArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
