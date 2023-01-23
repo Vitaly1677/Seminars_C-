3f8ed4c813c871f406d8b2af2e212b2f0afee7b2
Vitaly Adass <geekpython16@yandex.ru>
2023-01-23 12:59:13 +0300

    Заполнение и печать массива взял из домашней задачи Task29

--- Task31_массив_сумма_отр_и_полож/Program.cs
+++ Task31_массив_сумма_отр_и_полож/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный
+// случайными числами из промежутка [-9, 9]. 
+
+// Найдите сумму отрицательных и положительных элементов массива.
+
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] 
+// сумма положительных чисел равна   29, 
+// сумма отрицательных равна        -20.
+
+int[] array = CreateArray(12,-9,9);
+PrintArray(array);
+int[] CreateArray(int size, int rndBegin, int rndEnd)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(rndBegin, rndEnd);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i != 0) Console.Write(", ");
+        Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
