ab966a7e11833c701ea9fc14009fe9cf7e5b1378
Vitaly Adass <geekpython16@yandex.ru>
2023-01-25 22:56:05 +0300

    Добавил решение Task42

---
+++ Task42_Десятичные_в_двоичные/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Write("Введите число, которое будет преобразовано в двоичное: ");
+
+int decNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ConvertDecToBin(decNumber));
+
+string ConvertDecToBin(int num)
+{
+    string binString = string.Empty;
+    while (num > 0)
+    {
+        binString = num % 2 + binString;
+        num = num / 2;
+    }
+    return binString;
+}
