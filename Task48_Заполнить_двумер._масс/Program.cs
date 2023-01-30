3a22ab9bceb6a652ea21a5c2f32d0db27d1f5c0e
Vitaly Adass <geekpython16@yandex.ru>
2023-01-30 14:13:12 +0300

    Добавил решение Task48

--- Task48_Заполнить_двумер._масс/Program.cs
+++ Task48_Заполнить_двумер._масс/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] array2D = CreateMatrixInt(3, 4);
+PrintArray(array2D);
+
+int[,] CreateMatrixInt(int rows, int column)
+{
+    int[,] matrix = new int[rows, column];
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i+j;
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 3},");
+            else Console.Write($"{matrix[i, j], 3}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
