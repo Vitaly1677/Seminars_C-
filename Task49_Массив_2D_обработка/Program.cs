58b060626da53334158736ec8d93d18a38692155
Vitaly Adass <geekpython16@yandex.ru>
2023-01-31 10:43:30 +0300

    Добавил решение Task49

--- Task49_Массив_2D_обработка/Program.cs
+++ Task49_Массив_2D_обработка/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Новый массив будет выглядеть
+// вот так:
+// 1 4 49 2
+// 5 9 2 3
+// 64 4 4 4
+
+int[,] matrix = new int[,] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+Console.WriteLine("Начальный массив");
+PrintArray(matrix);
+MatrixProcessing(matrix);
+
+Console.WriteLine("Новый массив");
+PrintArray(matrix);
+MatrixProcessing(matrix);
+
+void MatrixProcessing(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (NumEven(i) && NumEven(j)) matr[i, j] = matr[i, j] *= matr[i, j];
+        }
+    }
+}
+
+bool NumEven(int num)
+{
+    return num % 2 == 0;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3},");
+            else Console.Write($"{matrix[i, j],3}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
