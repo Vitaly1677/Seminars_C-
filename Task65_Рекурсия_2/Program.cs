11dc7edf021f9da754111fad9b4f6ba025901308
Vitaly Adass <geekpython16@yandex.ru>
2023-02-06 11:03:23 +0300

    Добавил решение

--- Task65_Рекурсия_2/Program.cs
+++ Task65_Рекурсия_2/Program.cs
@@ -2,3 +2,26 @@
 // выведет все натуральные числа в промежутке от M до N.
 // M = 1; N = 5 -> "1, 2, 3, 4, 5"
 // M = 4; N = 8 -> "4, 5, 6, 7, 8"
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbersRange(firstNumber, secondNumber);
+
+void NaturalNumbersRange(int firstNum, int secondNum)
+{
+if (firstNum == secondNum) Console.Write($"{firstNum} ");
+else if (firstNum < secondNum)
+{
+Console.Write($"{firstNum} ");
+NaturalNumbersRange(firstNum + 1, secondNum);
+
+}
+else if (firstNum > secondNum)
+{
+Console.Write($"{firstNum} ");
+NaturalNumbersRange(firstNum - 1, secondNum);
+
+}
+}
