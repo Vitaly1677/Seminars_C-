c5c4e998e5b46cdbf60167cbc6246371208bb0e9
Vitaly Adass <geekpython16@yandex.ru>
2023-02-06 10:13:21 +0300

    Добавил задание

--- Task63_Рекурсия/Program.cs
+++ Task63_Рекурсия/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
